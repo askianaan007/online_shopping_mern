78e31c5ea946d000c89ee8c849ca69ac1f09bc92
aski anaan <askianaan007@gmail.com>
2024-10-16 12:17:40 +0530

    filterapi options completed

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const products = require("./routes/product");
+const auth = require("./routes/auth");
 const errorHandler = require("./middlewares/error");
 
 const app = express();
 
 app.use(express.json());
 app.use("/api/v1/", products);
+app.use("/api/v1/", auth);
 app.use(errorHandler);
 
 module.exports = app;
